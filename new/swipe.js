6a5011bd3777d58874f36aade6a7a2603ffba3d9
lumb <67676290+Lumbarque-Oficial@users.noreply.github.com>
2020-08-28 01:21:43 -0300

    Add files via upload
    add touch events

---
+++ new/swipe.js
@@ -0,0 +1,32 @@
+document.addEventListener('swiped-left', function(e) {
+     console.log(e.type);
+     console.log(e.target);
+     if (e.target == document.getElementById('canvas')) {
+        snake.xSpeed = scale*-1;
+		snake.ySpeed = 0;	
+     }
+});
+document.addEventListener('swiped-right', function(e) {
+     console.log(e.type);
+     console.log(e.target);
+     if (e.target == document.getElementById('canvas')) {
+        snake.xSpeed = scale*1;
+        snake.ySpeed = 0;   
+     }
+});
+document.addEventListener('swiped-up', function(e) {
+     console.log(e.type);
+     console.log(e.target);
+     if (e.target == document.getElementById('canvas')) {
+        snake.xSpeed = 0;
+        snake.ySpeed = scale*-1;   
+     }
+});
+document.addEventListener('swiped-down', function(e) {
+     console.log(e.type);
+     console.log(e.target);
+     if (e.target == document.getElementById('canvas')) {
+        snake.xSpeed = 0;
+        snake.ySpeed = scale*1;   
+     }
+});
